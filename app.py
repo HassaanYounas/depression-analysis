ac4713139f4c047a76c976ad1ce09269fdf24115
Hassaan Younas <hassaanyounas123@gmail.com>
2021-06-27 19:03:00 +0500

    Checking.

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-import json, analysis
+# import json, analysis
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/api/analysis', methods = ['POST'])
-def ner():
-    handle = request.get_json().get('TwitterHandle', '')
-    index = analysis.depression_analysis(handle)
-    response = app.response_class(
-        response = json.dumps(index),
-        status = 200,
-        mimetype = 'application/json'
-    )
-    return response
+# @app.route('/api/analysis', methods = ['POST'])
+# def ner():
+#     handle = request.get_json().get('TwitterHandle', '')
+#     index = analysis.depression_analysis(handle)
+#     response = app.response_class(
+#         response = json.dumps(index),
+#         status = 200,
+#         mimetype = 'application/json'
+#     )
+#     return response
 
 if __name__ == '__main__':
     app.run()
